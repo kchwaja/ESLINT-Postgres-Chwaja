ef344f661ea203e8cc48bc002d9a7976d97a3fd9
Krystian <chwaja.k03@htlwienwest.at>
2021-05-25 09:08:36 +0200

    Aufgabe 1 erledigt

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
-// const asyncHandler = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
 
 const router = express.Router();
+
+const { getCocktails } = require('../model/cocktails');
+
+router.get(
+  '/cocktails',
+  asyncHandler(async (req, res) => {
+    const result = await getCocktails();
+    res.status(result.code).json(result);
+  }),
+);
 
 module.exports = router;
